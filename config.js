0158659729b99d79e75adaecd7d64d3212a07ac2
Avishi-KVIISc <82009227+Avishi-KVIISc@users.noreply.github.com>
2021-12-01 20:38:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,17 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+  apiKey: "AIzaSyAc8DNqcHit6DXluQ5qY5wfwBxcIqSR5QE",
+
+  authDomain: "project-67-ce450.firebaseapp.com",
+
+  projectId: "project-67-ce450",
+
+  storageBucket: "project-67-ce450.appspot.com",
+
+  messagingSenderId: "1070189548596",
+
+  appId: "1:1070189548596:web:a106e393395c60cd3ce661"
 
 };
 // Initialize Firebase
